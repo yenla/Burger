53229ab05349757b286f63546e956891ec53465c
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-05-05 15:54:27 -0700

    minor update to server js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "skye123",
   database: "burgers_db"
 });
 
