05911968f7d6d582b5f11847f31bc72baeff333f
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-05-05 16:20:44 -0700

    revise connection js with heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "skye123",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "skye123",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+	  	user: "root",
+	  	password: "skye123",
+	  	database: "burgers_db"
+	});
+};
 
 connection.connect(function(err){
 	if (err) {
