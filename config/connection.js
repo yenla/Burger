716d31185bd6d086da0431e09d9479f3f95f82c3
Skyler La <yenla@skylers-mbp.attlocal.net>
2017-05-05 23:00:31 -0700

    update connection js

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: "localhost",
 	  	user: "root",
-	  	password: "skye123",
+	  	password: "",
 	  	database: "burgers_db"
 	});
 };
